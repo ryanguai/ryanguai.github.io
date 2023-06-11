7790b92b9d4ef20d01bde969a0bbaa7afdf129f7
ryanguai <e0694442@u.nus.edu>
2023-06-11 23:27:15 +0800

    Finalise UI for writing section page

--- script.js
+++ script.js
@@ -37,3 +37,51 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const articles = [
+      {
+        image: 'socl22.jpg',
+        description: 'School of Christian Leadership 2022: Called & Set Apart',
+        path: 'socl22.html'
+      },
+      {
+        image: 'socl23.jpg',
+        description: 'School of Christian Leadership 2023'
+      }
+      // Add more articles as needed
+    ];
+  
+    let currentIndex = 0; // Initialize the current article index
+  
+    const articleImage = document.getElementById('article-image');
+    const articleDescription = document.getElementById('article-description');
+    const articlePath = document.getElementById('article-path');
+    const arrowLeft = document.querySelector('.arrow-left');
+    const arrowRight = document.querySelector('.arrow-right');
+  
+    // Function to update the current article
+    function updateArticle() {
+      const currentArticle = articles[currentIndex];
+      articleImage.src = currentArticle.image;
+      articleImage.alt = `Writing ${currentIndex + 1}`;
+      articleDescription.textContent = currentArticle.description;
+      articlePath.href = currentArticle.path;
+    }
+  
+    // Event listener for the left arrow
+    arrowLeft.addEventListener('click', function() {
+      currentIndex = (currentIndex - 1 + articles.length) % articles.length;
+      updateArticle();
+    });
+  
+    // Event listener for the right arrow
+    arrowRight.addEventListener('click', function() {
+      currentIndex = (currentIndex + 1) % articles.length;
+      updateArticle();
+    });
+  
+    // Initial update to display the first article
+    updateArticle();
+  });
+  
